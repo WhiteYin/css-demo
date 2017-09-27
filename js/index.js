02263252e9aa1e5288d0c6e2e0a0096fbfc784e8
WhiteYin <yinoo0@163.com>
2017-09-27 10:13:00 +0800

    “背景位置和是否重复。重新布局了舞台。”

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ $(document).ready(
     function () {
         var navBox = $("#nav-box");
         var box = $('#show-box');
+        var des = $('#description');
+        var style = $('#editor');
         var boxClass = '';
 
         //当鼠标移入下拉框区域，新增类样式.show
@@ -17,8 +19,10 @@ $(document).ready(
             var that = $(this);
             //下拉框消失
             navBox.removeClass("show");
+            //取选项的内容冒号前的部分，否则内容过长导致选择栏变形
+            var text = that.text().split(':')[0];
             //文本更改为选项文本
-            $('#nav-text').text(that.text());
+            $('#nav-text').text(text);
             boxClass = that.attr('data-class');
             //清除原有的类
             box.attr('class', '');
