c89c116ed2cdf68131e1f5e47fe8edf713cf8a1e
WhiteYin <yinoo0@163.com>
2017-10-11 17:30:55 +0800

    canvas的部分接口

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ $(document).ready(
         var des = $('#description');
         var style = $('#editor');
         var boxClass = '';
+        var canvas = document.querySelector("#canvas");
+        var context = canvas.getContext("2d");
 
         //当鼠标移入下拉框区域，新增类样式.show
         navBox.bind('mouseenter', function () {
@@ -28,6 +30,58 @@ $(document).ready(
             box.attr('class', '');
             //展示div块添加指定的类
             box.addClass(boxClass);
+        });
+        //绘制直线
+        $('#draw-lines').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.strokeStyle = '#f00';
+            context.moveTo(40, 220);
+            context.lineTo(100, 80);
+            context.lineTo(350, 180);
+            context.stroke();
+        });
+        //绘制三角形
+        $('#draw-triangle').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.strokeStyle = '#0f0';
+            context.lineJoin = 'round';
+            context.moveTo(50,50);
+            context.lineTo(250,350);
+            context.lineTo(250, 50);
+            context.closePath();
+            context.stroke();
+        });
+        //绘制矩形
+        $('#draw-rect').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.strokeStyle = '#aaf';
+            context.strokeRect(50, 50, 200, 200);
+            context.fillStyle = '#028';
+            context.fillRect(100, 100, 150, 150);
+            context.clearRect(180, 180, 50, 50);
+        });
+        //绘制圆形
+        $('#draw-circle').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.arc(100, 100, 50, 0, (3 / 2) * Math.PI, false);
+            context.stroke();
+
+            context.beginPath();
+            context.arc(300, 300, 50, (4 / 3) * Math.PI, true);
+            context.fill();
+        });
+        //使用arcTo绘制圆弧
+        $('#draw-circle-arcto').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.moveTo(200, 100);
+            context.lineTo(300, 100);
+            context.arcTo(100, 100, 200, 400, 50);
+            context.stroke();
         });
     }
 );
