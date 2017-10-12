a398cdc5ffea0c1e94b662d8d8d0ececc5019467
WhiteYin <yinoo0@163.com>
2017-10-12 12:59:04 +0800

    canvas 文字和图片

--- js/index.js
+++ js/index.js
@@ -41,6 +41,54 @@ $(document).ready(
             context.lineTo(350, 180);
             context.stroke();
         });
+        //绘制不同粗细的直线
+        $('#linewidth-lines').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            
+            context.beginPath();
+            context.lineWidth = 5;
+            context.moveTo(10, 100);
+            context.lineTo(300, 100);
+            context.stroke();
+
+            context.beginPath();
+            context.lineWidth = 10;
+            context.moveTo(10, 200);
+            context.lineTo(300, 200);
+            context.stroke();
+            
+            context.beginPath();
+            context.lineWidth = 20;
+            context.moveTo(10, 300);
+            context.lineTo(300, 300);
+            context.stroke();
+        });
+        //绘制字母M
+        $("#draw-m").bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.lineWidth = 10;
+            context.lineJoin = 'bavel';
+            context.lineCap = 'round';
+            
+            context.moveTo(100, 350);
+            context.lineTo(150, 100);
+            context.lineTo(200, 350);
+            context.lineTo(250, 100);
+            context.lineTo(300, 350);
+            context.stroke();
+        });
+        //绘制虚线
+        $("#draw-dash").bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.lineWidth = 10;
+
+            context.setLineDash([20, 10,10,10]);
+            context.moveTo(100, 200);
+            context.lineTo(300, 200);
+            context.stroke();
+        });
         //绘制三角形
         $('#draw-triangle').bind('click', function () {
             context.clearRect(0, 0, canvas.width, canvas.height);
@@ -83,5 +131,60 @@ $(document).ready(
             context.arcTo(100, 100, 200, 400, 50);
             context.stroke();
         });
+        //文本
+        $('#draw-text').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            var text = "HELLO WORLD";
+            context.font = "bold 20px 微软雅黑";
+            context.textAlign = "left";
+            context.strokeText(text, 100, 100);
+            context.strokeText(text, 100, 150, 50);
+            context.fillText(text, 100, 200);
+            context.beginPath();
+            context.textAlign = "center";
+            context.strokeText(text, 100, 250);
+        });
+        //图片
+        $('#draw-img').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+
+            var img = new Image();
+            img.src = '../img/avatar.jpg';
+            //图片加载完再绘画
+            img.onload = function () {
+                context.drawImage(img, 200, 200, 100, 100);
+                context.drawImage(img, 0, 0, 100, 100, 100, 100,100, 100);
+            }
+        });
+        //图片平铺
+        $('#draw-img-repeat').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+
+            var img = new Image();
+            img.src = '../img/avatar.jpg';
+            //图片加载完再绘画
+            img.onload = function () {
+                var pattern = context.createPattern(img, 'repeat');
+                context.fillStyle = pattern;
+                context.fillRect(0, 0, canvas.width, canvas.height);
+            }
+        });
+        //图片裁剪
+        $('#clip-img').bind('click', function () {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.arc(200, 200, 100, 0, 2 * Math.PI);
+            
+            context.clip();
+
+            var img = new Image();
+            img.src = '../img/avatar.jpg';
+            img.onload = function () {
+                context.drawImage(img, 100, 100);
+            }
+        })
     }
 );
